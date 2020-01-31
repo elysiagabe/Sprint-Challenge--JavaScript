b5c90467e9ebba003606407662e9c1814bf3287b
Elysia Gabe <elysiagabe@gmail.com>
2020-01-31 08:38:29 -0800

    Answers Request 1 of arrays in objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -19,7 +19,7 @@ const dino1 = {
   diet: 'carnivorous',
   weight: '7000kg',
   length: '12m',
-  period: 'Late Cretaceous'
+  period: 'Late Cretaceous',
   speak: function(){
     return 'RAWERSRARARWERSARARARRRR!';
   }
@@ -78,8 +78,12 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+
+const universities = graduates.map((graduate) => {
+  return graduate.university;
+});
+
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
