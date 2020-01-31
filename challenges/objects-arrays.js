8f1f245c6b6082143b6af84c9fd502fa36e5a6bc
Elysia Gabe <elysiagabe@gmail.com>
2020-01-31 08:51:38 -0800

    Answers Arrays Request 3 in objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -82,7 +82,6 @@ Once you have the new array created, sort the universities alphabetically and lo
 const universities = graduates.map((graduate) => {
   return graduate.university;
 });
-
 console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -97,7 +96,9 @@ const contactInfo = grads.map((graduate) => {
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter((graduate) => {
+  return graduate.university.includes("Uni");
+});
 console.log(unisWithUni);
 
 
